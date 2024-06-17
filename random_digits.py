af6ddd4da8c1199b869dae2deca8c722d9c469eb
Eric HB <107995930+eric-hb-carwow@users.noreply.github.com>
2024-06-17 13:44:37 +0100

    Add any number of groups

--- random_digits.py
+++ random_digits.py
@@ -5,10 +5,12 @@ def main():
     st.title("Press for a randomised list of last X digits in the user id")
     st.write("If there are any problems, you reach out to the analytics team and give them this URL which runs the code: https://github.com/eric-hb-carwow/crm_random_digits_regex")
 
-    col1,_ = st.columns([1,2])
+    col1,col2,_ = st.columns([1,1,1])
     
     with col1:
         num_digits = st.number_input("Number of digits to randomise", min_value=1, max_value=4, step=1, value=2)
+    with col2:
+        num_groups = st.number_input("Number of groups", min_value=1, step=1, value=2)
     
     if st.button('Randomise!'):
         
@@ -19,15 +21,21 @@ def main():
         random.shuffle(all_combinations)
         
         # Divide the list into control and test groups
-        mid_point = len(all_combinations) // 2
-        control_group = all_combinations[:mid_point]
-        test_group = all_combinations[mid_point:]
-        
+        start = 0
+        mid_point = len(all_combinations) // num_groups
+        for i in range(num_groups-1):
+            group = all_combinations[start:mid_point]
+            start += mid_point
+            mid_point += mid_point
+            
+            if i = 0:
+                st.write(f'Group 1, note the copy button on the right side!')
+            else 
+                st.write(f"Group %s" % (i+1))
+
         # Display the results
-        st.write('Group 1, note the copy button on the right side!')
-        st.code(f".*{'|.*'.join(control_group)}")
-        st.write('Group 2')
-        st.code(f".*{'|.*'.join(test_group)}")
+        st.code(f".*{'|.*'.join(group)}")
+        
 
 if __name__ == "__main__":
     main()
